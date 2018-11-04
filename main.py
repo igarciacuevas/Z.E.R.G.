01fe8af35799c5953e48095c138f63d4190adddd
igarciacuevas <44346224+igarciacuevas@users.noreply.github.com>
2018-11-04 12:47:29 +0100

    Update main.py
    Inicio de la implementacion de la inicializacion de enjambre

--- main.py
+++ main.py
@@ -6,12 +6,29 @@ Created on Sat Nov  3 21:12:33 2018
 """
 
 import math
+import random
 from Inicializar import *
+import GenParticula
 
 
 #Lectura de datos
 Citys=leerdatos("berlin52.txt")
 #Calculo de distancias
 Distancias=CalcularDistancias(Citys["Lat"],Citys["Long"])
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                   ENJAMBRE
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Variables iniciales y auxiliares
+tamaño=500
+numvecinos=4
+itermax=600
+enjambre = []
+indices = list(range(tamaño - 1))
+
+# Vamos a generar todas las particulas iniciales
+for i in range(len(tamaño)):
+  indicesvecinos = random.sample(indices, numvecinos) # Vecinos para cada particula con los que realizará crossovers
+  particula = GenParticula(
 
 
