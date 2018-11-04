594227de90c0e670eca1c534458d20dc573d3215
igarciacuevas <44346224+igarciacuevas@users.noreply.github.com>
2018-11-04 14:18:12 +0100

    Add files via upload
    Funcion para elegir el mejor de los vecinos de la particula seleccionada. Una vez elegida, ambas particulas podrán recombinarse para dar lugar a una nueva particula. Dicha nueva particula también podrá sufrir mutaciones

---
+++ Evolucion.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  4 13:19:27 2018
+
+@author: igarc
+"""
+
+import Crossovers
+    
+def gennuevaparticula(enjambre,particula,operador_cross,operador_mut):
+    # Sacamos los indices de los vecinos
+    indicesvecinos = particula.vecinos
+    # Seleccionamos el mejor de los vecinos, el cual sera utilizado para el crossover
+    mejorvecino = None # = None para la primera iteracion
+    for idx in indicesvecinos:
+        temp_particula = enjambre[idx]
+        if (mejorvecino == None or temp_particula.distanciaruta < mejorvecino.distanciaruta):
+            mejorvecino = temp_particula
+        
+    # Una vez seleccionado el mejor vecino, realizamos el crossover
+    nuevaparticula = cruzarymutar(particula,mejorvecino,operador_cross,operador_mut)
+    
+    
