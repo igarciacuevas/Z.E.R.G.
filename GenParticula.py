94a7fdbb840097645afad38fcc52c8b1e805d9c5
igarciacuevas <44346224+igarciacuevas@users.noreply.github.com>
2018-11-13 18:42:17 +0100

    Expropiacion de variables

--- GenParticula.py
+++ GenParticula.py
@@ -12,20 +12,20 @@ class GenParticula:
     def __init__(self, ruta, distancias, vecinos):
         self.ruta = ruta # Orden de ciudades de la ruta
         self.vecinos = vecinos # Indices de los vecinos con los que realizar crossover
-        self.__distancias = distancias # Distancias entre ciudades
+        self.distancias = distancias # Distancias entre ciudades
         self.distanciaruta = 0.0 # Distancia de la ruta creada
-        self.__calculardistanciaruta() # Calculo de la distancia de la ruta creada
+        self.calculardistanciaruta() # Calculo de la distancia de la ruta creada
         
     # Calcular la distancia de la ruta creada
-    def __calculardistanciaruta(self):
+    def calculardistanciaruta(self):
         # Iteramos a lo largo de la ruta
         for idx in range(len(self.ruta) - 1):
             iorigen = self.ruta[idx] # Indice ciudad origen
             idestino = self.ruta[idx + 1] # Indice ciudad destino
             # Acumulacion de distancias
-            self.distanciaruta += self.__distancias[iorigen][idestino] 
+            self.distanciaruta += self.distancias[iorigen][idestino] 
         # Añadimos la ultima distancia desde el punto final al punto inicial de la ruta
-        self.distanciaruta += self.__distancias[self.ruta[len(self.ruta) - 1]][self.ruta[0]]
+        self.distanciaruta += self.distancias[self.ruta[len(self.ruta) - 1]][self.ruta[0]]
         return self.distanciaruta
     
 # Llamada a esta clase
