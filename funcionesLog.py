3714acb5e2a69be0874e6d2355d931cfeba90283
igarciacuevas <44346224+igarciacuevas@users.noreply.github.com>
2018-11-17 20:49:19 +0100

    Funcion logfile
    Funcion que escribe las condiciones de la ejecucion de la búsqueda en el logfile de dicha ejecucion. Se recoge:
    + Tamaño del enjambre
    + Numero de iteraciones máximas
    + Operadores crossover y mutacion elegido
    + Mejor distancia del enjambre tras la inicializacion del mismo, antes de empezar la evolucion de sus particulas

---
+++ funcionesLog.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 17 17:58:15 2018
+
+@author: igarc
+
+Funcion para incializar el log file, escribiendo las condiciones del experimento
+"""
+
+
+def iniciar(archivo,cross,mut,tamaño,iteraciones):
+    archivo.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    archivo.write("\n")
+    archivo.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    archivo.write("\n")
+    archivo.write("\t" + "CONDICIONES DEL EXPERIMENTO:")
+    archivo.write("\n")
+    archivo.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    archivo.write("\n")
+    archivo.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    archivo.write("\n")
+    
+    archivo.write("El enjambre tiene " + str(tamaño) + " particulas.")
+    archivo.write("\n")
+    archivo.write("La busqueda tendrá como máximo " + str(iteraciones) + " iteraciones.")
+    archivo.write("\n")
+    
+    if cross == 2: #Cycle
+        archivo.write("El operador de crossover es Cycle.")
+        archivo.write("\n")
+    elif cross == 3: #PMX
+        archivo.write("El operador de crossover es PMX.")
+        archivo.write("\n")
+    else: #Order1
+        archivo.write("El operador de crossover es Oder1.")
+        archivo.write("\n")
+    
+    if mut == 1: #Inversion
+        archivo.write("El operador de mutacion es Inversion.")
+        archivo.write("\n")
+    elif cross == 2: #Swap
+        archivo.write("El operador de crossover es SingleSwap.")
+        archivo.write("\n")
+    else: #Nothing
+        archivo.write("No hay operador de mutacion.")
+        archivo.write("\n")
+    
+    archivo.write("\n")
+    
+    archivo.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    archivo.write("\n")
+    archivo.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    archivo.write("\n")
+    archivo.write("\t" + "MEJOR DISTANCIA PARA CADA ITERACION:")
+    archivo.write("\n")
+    archivo.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    archivo.write("\n")
+    archivo.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    archivo.write("\n")
+    
+    archivo.write("\n")
+    archivo.write("\n")
+    
+    return
+    
+    
