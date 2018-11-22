ca8659d934bb4b9b012e8819b320fd354325a226
igarciacuevas <44346224+igarciacuevas@users.noreply.github.com>
2018-11-22 22:46:26 +0100

    BugFix + CambioFuncion
    SingleSwap ==> RandomSwap
    Dada la gran magnitud de algunos problemas, el intercambiar un solo elemento se torna de poca utilidad
    Bugfix en la funcion de inversion

--- Mutations.py
+++ Mutations.py
@@ -9,7 +9,7 @@ http://www.rubicite.com/Tutorials/GeneticAlgorithms.aspx
 
 2 doperadores de mutacion han sido seleccionados:
     Inversion
-    SingleSwap
+    RandomSwap
 """
 
 import random
@@ -20,23 +20,55 @@ https://www.youtube.com/watch?v=WSx_l1Km72U
 """
 def Inversion(ruta):
     # Posiciones de inversion
-    pos1 = random.randint(0,len(ruta)-1)
-    pos2 = random.randint(0,len(ruta)-1)
+    posorigen = random.randint(0,len(ruta)-1)
+    posfinal = random.randint(0,len(ruta)-1)
+    
+    # si la posicion final es antes que la del origen, la instercambiamos
+    if posfinal < posorigen: 
+        posorigen, posfinal = posfinal, posorigen
+    elif posorigen == posfinal:
+        # Devolvemos la original
+        return ruta
     
     # Invertimos
-    ruta[pos1:pos2] = ruta[pos1:pos2][::-1]
+    ruta[posorigen:posfinal+1] = ruta[posorigen:posfinal+1][::-1] #List indexes are weird
     
     return ruta
 
 """
-Intercambiamos dos posiciones de sitio
+Intercambiamos posiciones de sitio
 """
-def SingleSwap(ruta):
-    # Posiciones de intercambio
-    pos1 = random.randint(0,len(ruta)-1)
-    pos2 = random.randint(0,len(ruta)-1)
+def RandomSwap(ruta):
+    # Posicion limite
+    poslimit = random.randint(0,len(ruta)-1)
     
-    # Intercambiamos
-    ruta[pos1],ruta[pos2] = ruta[pos2],ruta[pos1]
+    # Limite en el borde - devolver ruta original
+    if poslimit == 0 or poslimit==(len(ruta)-1):
+        return ruta
+    
+    # Tamaño maximo de rango
+    if poslimit > (len(ruta)-1-poslimit):
+        maxrango = (len(ruta)-1-poslimit)
+    else:
+        maxrango = poslimit
+    
+    # Posiciones izquieda
+    pos1I = random.randint(0,poslimit)
+    tamaño = random.randint(0,maxrango)
+    
+    # Limites
+    if (pos1I+tamaño) > poslimit:
+        pos1I = poslimit-tamaño
+        
+    # Posiciones derecha
+    pos1D = random.randint(poslimit+1,len(ruta)-1)
+    
+    # Limites
+    if (pos1D+tamaño) > (len(ruta)-1):
+        pos1D = (len(ruta)-1)-tamaño
+        
+    # Intercambio
+    for i in range(tamaño):
+        ruta[pos1I+i] , ruta[pos1D+i] = ruta[pos1D+i] , ruta[pos1I+i]
     
     return ruta
